dbf7c341d912262d17f1f6aa7b3799eafd3abec7
Giacomo Stefanizzi <giacomo.stefanizzi@mail.polimi.it>
2022-05-09 22:49:52 +0200

    Added ClientCloud, ClientPlayer, ClientSchoolBoard for client minimodel. Implemented related ModelChange packets. Added some getter and setter methods to make them work. Uploaded PeerReview 2

--- src/main/java/modelChange/PlayerChange.java
+++ src/main/java/modelChange/PlayerChange.java
@@ -1,12 +1,16 @@
 package modelChange;
 
 import client.model.ClientGameBoard;
+import client.model.ClientSchoolBoard;
 import server.model.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlayerChange extends ModelChange{
 
     private String name;
-    private SchoolBoard schoolBoard;
+    private ClientSchoolBoard schoolBoard;
     private int coins;
     private TowerColor towerColor;
     private AssistantType assistantType;
@@ -15,10 +19,43 @@ public class PlayerChange extends ModelChange{
 
     @Override
     public void execute(ClientGameBoard gameBoard){
-        /*TODO gameBoard.getPlayer(Player.getName()).setSomeThing(this.someThing)*/
+        gameBoard.getPlayer(name).setSchoolBoard(schoolBoard);
+        gameBoard.getPlayer(name).setCoins(coins);
+        gameBoard.getPlayer(name).setTowerColor(towerColor);
+        gameBoard.getPlayer(name).setAssistantType(assistantType);
+        gameBoard.getPlayer(name).setPlayedAssistant(playedAssistant);
+        gameBoard.getPlayer(name).setAssistants(assistants);
     }
 
     public PlayerChange(Player player){
-        /*TODO this.someThing=player.getSomeThing*/
+
+        this.name = player.getName();
+
+        //setting schoolBoard attribute
+        ClientSchoolBoard clientSchoolBoard = new ClientSchoolBoard();
+
+        clientSchoolBoard.setEntrance(player.getSchoolBoard().getStudentsInEntrance());
+        clientSchoolBoard.setNumOfTowers(player.getSchoolBoard().getNumOfTowers());
+        clientSchoolBoard.setTowersColor(player.getSchoolBoard().getTowersColor());
+
+        Map<Color, Integer> diningRoom = new HashMap<>();
+        for(Color c : Color.values()){
+            diningRoom.put(c,player.getSchoolBoard().getNumOfStudentsInDining(c));
+        }
+        clientSchoolBoard.setDiningRoom(diningRoom);
+
+        Map<Color, Integer> professors = new HashMap<>();
+        for(Color c : player.getSchoolBoard().getProfessorsColor()){
+            professors.put(c,1);
+        }
+
+        clientSchoolBoard.setProfessors(professors);
+
+        //setting others player attributes
+        this.coins = player.getCoins();
+        this.towerColor = player.getTowerColor();
+        this.assistantType = player.getAssistantType();
+        this.playedAssistant = player.getPlayedAssistant();
+        this.assistants = player.getAssistants();
     }
 }
